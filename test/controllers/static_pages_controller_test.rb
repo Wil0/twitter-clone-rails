929a16613b743d04e2bf22612a57660a529ba17b
emilio <emsmnndz@gmail.com>
2016-07-26 18:07:34 +0100

    adds helper method fot apllication

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def text
-    text = '| Ruby on Rails Tutorial Sample App'
+    text = 'Ruby on Rails Tutorial Sample App'
   end
 
   test "should get root route" do
@@ -14,25 +14,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home #{text}"
+    assert_select "title", :content => text
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help #{text}"
+    assert_select "title", :content => text
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About #{text}"
+    assert_select "title", :content => text
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact #{text}"
+    assert_select "title", :content => text
   end
 
 end
