ffcce287297e308d7e9eb3fae110bebcc287ce73
emilio <emsmnndz@gmail.com>
2016-07-28 17:11:03 +0100

    adds user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: {minimum: 3, maximum: 5}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: 6}
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Pepe", email: 'pepe@email')
-  end
-
-  test "user has name and email to be valid" do
-    assert @user.valid?
+    @user = User.new(name: "Pepe", email: 'pepe@email.com',
+                  password: "foobar", password_confirmation: "foobar")
   end
 
   test "name should be present" do
@@ -15,8 +12,53 @@ class UserTest < ActiveSupport::TestCase
     refute @user.valid?
   end
 
+  test "name should be no longer than 50 characters" do
+    @user.name = 'P' * 51
+    refute @user.valid?
+  end
+
+  test "name should be no shorter than 3 characters" do
+    @user.name = 'P'
+    refute @user.valid?
+  end
+
   test "email should be present" do
     @user.email = ''
     refute @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test 'email must be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+   @user.password = @user.password_confirmation = " " * 6
+   assert_not @user.valid?
+ end
+
+ test "password should have a minimum length" do
+   @user.password = @user.password_confirmation = "a" * 5
+   assert_not @user.valid?
+ end
 end
